1cf8a650dafa33f487e450b40afa3c8246a05b08
Priya Prakash <65969371+Priyaprak@users.noreply.github.com>
2020-09-23 17:37:33 +0100

    Update schedulescontroller.js
    Removed reference codes.

--- controllers/schedulescontroller.js
+++ controllers/schedulescontroller.js
@@ -2,8 +2,7 @@ const schedulesmodel= require('../model/schedulesmodel');
 const schedulescontroller={
     /* Display list of schedules. */
     listAllSchedules:(req,res) =>{
-       // db.query("SELECT * FROM schedules", function(err,result){
-        schedulesmodel.retrieveData((result)=>{
+       schedulesmodel.retrieveData((result)=>{
             res.render('index', {
                 title:'Schedule Management of Users',
                 result, 
@@ -23,20 +22,16 @@ const schedulescontroller={
     submitNewSchedule :(req,res)=>{
         /*Validate the differnce in time values of the form is greater than 5 hours.*/
         schedulesmodel.validateTime([req.body.starttime,req.body.endtime],(result)=>{
-           // console.log("Object values are:"+Object.values(result[0]));
             if(result>='05:00:00'){
-               console.log("Values are "+req.body.username+req.body.dayoftheweek+req.body.starttime+req.body.endtime)
                 schedulesmodel.insertData([req.body.username, req.body.dayoftheweek, req.body.starttime, req.body.endtime],(result)=> {
-                console.log("After inserting no of Rows inserted into table successfully is : " +result.affectedRows);
                     res.redirect('/');
-                    
                 });
             }else{
-                console.log("LEsser than 5 hours");
-                res.render('form',
-                {title:'Create New Schedule',msg:'Please specify valid shift timings OF 5 hrs.'});
+                 res.render('form',
+                {   title:'Create New Schedule',
+                    msg:'Please specify valid shift timings OF 5 hrs.'
+                });
             }      
-            
         });
     }
 };
